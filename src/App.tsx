2f55672d42a0b8c172851d6e1b42a8e2a110b2e2
Uditt Lamba <uditt.lamba@pm.me>
2022-07-04 12:06:13 +0530

    1. moved back to react front end. much easier this way.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import {CodeEditorBasic} from "./components/code-editor";
+
+function App() {
+  return (
+    <div className="App">
+      <CodeEditorBasic/>
+    </div>
+  );
+}
+
+export default App;
